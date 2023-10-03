16235203e9fbb85e42f3ce6866e88908d5bfbd16
vilenshakur <125366281+vilenshakur@users.noreply.github.com>
2023-10-03 20:53:47 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+ $(document).ready(function () {
+ $(".play-btn").click(function () {
+    $(".modal-calam ").toggleClass("hidden");
+  });
+  $(".modal-close").click(function () {
+    $(".modal-calam").toggleClass("hidden");
+  });
+});
